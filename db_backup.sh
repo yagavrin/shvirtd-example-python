7af8eb06e2d1be2ef9592159a5dcbf2d35233b86
yagavrin <62797682+yagavrin@users.noreply.github.com>
2025-01-26 01:26:16 +0300

    Update db_backup.sh

--- db_backup.sh
+++ db_backup.sh
@@ -1,13 +1,11 @@
 !/bin/bash
+DUMP_NAME="dump-$(date +"%d.%m.%y-%H.%M.%S").sql"
+
 if [ -f "$HOME/db.conf" ]; then
    . $HOME/db.conf
 else
    echo "file db.conf not found"
    exit 1
-fi
-
-DUMP_NAME="dump-$(date +"%d.%m.%y-%H.%M.%S").sql"
-
-mkdir -p /opt/backup
-
-docker run --rm --network shvirtd-example-python_backend mysql:8 mysqldump -h$DB_HOST -u$DB_USER -p$DB_PASS $DB_NAME > backup && sudo mv backup /opt/backup/$DUMP_NAME
+fi \
+&& mkdir -p /opt/backup \
+&& docker run --rm --network shvirtd-example-python_backend mysql:8 mysqldump -h$DB_HOST -u$DB_USER -p$DB_PASS $DB_NAME > backup && sudo mv backup /opt/backup/$DUMP_NAME
