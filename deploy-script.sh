d431cf0028b368b43eb8728451ce615ada82b08a
yagavrin <62797682+yagavrin@users.noreply.github.com>
2025-01-26 01:24:32 +0300

    Update deploy-script.sh

--- deploy-script.sh
+++ deploy-script.sh
@@ -1,5 +1,15 @@
-cd /opt \
-&& git clone https://github.com/yagavrin/shvirtd-example-python.git \
-&& cd shvirtd-example-python \
+#!/bin/bash
+REPO_DIR="/opt/shvirtd-example-python"
+REPO_URL="https://github.com/yagavrin/shvirtd-example-python.git"
+
+if [ -d "$REPO_DIR" ]; then
+  # If the directory exists, perform a git pull
+  echo "Repository exists. Pulling latest changes..."
+  cd "$REPO_DIR" && git pull
+else
+  # If the directory does not exist, perform a git clone
+  echo "Repository does not exist. Cloning..."
+  git clone "$REPO_URL" "$REPO_DIR" && cd "$REPO_DIR"
+fi \
 && docker build -t shvirtd-example-python-web:1.0.0 -f Dockerfile.python . \
 && docker compose up -d
