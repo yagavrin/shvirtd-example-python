09589c18a23268477b14c78ba6f02bf6c1e64e6f
yagavrin <62797682+yagavrin@users.noreply.github.com>
2025-01-25 22:48:27 +0300

    Update deploy-script.sh

--- deploy-script.sh
+++ deploy-script.sh
@@ -1,5 +1,5 @@
-cd /opt
-git clone https://github.com/yagavrin/shvirtd-example-python.git
-cd shvirtd-example-python
-docker build -t shvirtd-example-python-web:1.0.0 -f Dockerfile.python .
-docker compose up -d
+cd /opt \
+&& git clone https://github.com/yagavrin/shvirtd-example-python.git \
+&& cd shvirtd-example-python \
+&& docker build -t shvirtd-example-python-web:1.0.0 -f Dockerfile.python . \
+&& docker compose up -d
